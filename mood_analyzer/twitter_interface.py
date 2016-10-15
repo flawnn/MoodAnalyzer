8af56e7351a05e15c457990632238a54d3136ddb
NightScap3 <baran.peters@lessing-ffm.net>
2016-10-15 15:03:43 +0200

    Update Twitter_Interface.py

--- mood_analyzer/twitter_interface.py
+++ mood_analyzer/twitter_interface.py
@@ -1,20 +1,22 @@
-#from loklak import Loklak
+from loklak import Loklak
 
-
+loklak = Loklak()
 def get_tweets(hashtag):
     """
     Fetches all tweets to a given hashtag
     :param hashtag: (str) keyword
     :return: (list) list of json strings with content
     """
-
-    loklak = Loklak()
     tweet_list = []
 
     for t in loklak.search(hashtag)["statuses"]:
         tweet_list.append(t["text"])
 
     return tweet_list
+
+def get_status(self): 
+
+    print(loklak.status())
 
 
 if __name__ == "__main__":
