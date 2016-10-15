10a037b73338ac8bff2d07fa85e124fa75d3ce7c
moritz157 <moritz.ahrens2@gmail.com>
2016-10-15 17:32:49 +0200

    Add files via upload

--- mood_analyzer/templates/main.js
+++ mood_analyzer/templates/main.js
@@ -1,8 +1,11 @@
-var testData = {"mood": -83};
+var testData = {"positive": 1, "negative": 0};
 var negativeColorCode = "#ef5350";
 var negativePalette = "Reds";
+
 var positiveColorCode = "#66bb6a";
 var positivePalette = "Greens";
+
+var currentTheme = "";
 
 
 function getMood(){
@@ -12,7 +15,8 @@ function getMood(){
   if(hashtagInput.replace(" ", "").length>0){
     //Reset percent-circle
     document.getElementById("percent-circle").setAttribute("class", "big c100 p0");
-    document.getElementById("percent-span").innerHTML="0%";
+    document.getElementById("percent-span").childNodes[0].innerHTML="0%";
+    document.getElementById("percent-span").childNodes[2].innerHTML="0%";
     document.getElementById("percent-circle-wrapper").style.display="none";
 
     //Hide slogan
@@ -22,8 +26,9 @@ function getMood(){
     document.getElementById("preloader").style.display="block";
 
     //Placeholder
-    setTimeout(function(){receiveServerResponse(testData, "OK")}, 1000);
-    //$.post("100.100.219.101/hashtag",{"hashtag":hashtagInput}, receiveServerResponse(data, status));
+    //setTimeout(function(){receiveServerResponse(testData, "OK")}, 1000);
+    currentTheme=hashtagInput;
+    $.post("http://100.100.219.136/hashtag/",{"hashtag":hashtagInput}, receiveServerResponse);
   }else{
     console.log("Invalid user-input");
   }
@@ -35,8 +40,8 @@ function receiveServerResponse(data, status){
   console.log(data);
   document.getElementById("preloader").style.display="none";
   document.getElementById("percent-circle-wrapper").style.display="block";
-  var percent=data.mood;
-  if(data.mood>0){
+  var percent=Math.floor(data.positive/(data.positive+data.negative)*100);
+  if(percent>49){
     //Set the background to the defined positive color
     document.getElementById("body").style.backgroundColor=positiveColorCode;
     var pattern = Trianglify({
@@ -44,7 +49,7 @@ function receiveServerResponse(data, status){
       width: document.body.clientWidth,
       x_colors: positivePalette,
       cell_size: 40});
-  }else if(data.mood<0){
+  }else if(percent<50){
     //Set the background to the defined negative color
     document.getElementById("body").style.backgroundColor=negativeColorCode;
     var pattern = Trianglify({
@@ -52,8 +57,6 @@ function receiveServerResponse(data, status){
       width: document.body.clientWidth,
       x_colors: negativePalette,
       cell_size: 40});
-    //Invert the mood if it's negative
-    percent=percent*(-1);
   }
   document.body.appendChild(pattern.canvas());
   //Start the animation to display the percent
@@ -70,13 +73,25 @@ function setPercent(percent){
 
 function setPercentRaw(){
   var currentPercent = parseInt(document.getElementById("percent-circle").getAttribute("class").replace("big c100 p", ""));
+
+  if(currentPercent>50){
+    document.getElementById("fill").setAttribute("style", "border: 0.08em solid #66bb6a !important;");
+  }else{
+    document.getElementById("fill").setAttribute("style", "");
+  }
+
   if(currentPercent>percentToSet){
     document.getElementById("percent-circle").setAttribute("class", "big c100 p"+(currentPercent-1));
   }else if(currentPercent<percentToSet){
     document.getElementById("percent-circle").setAttribute("class", "big c100 p"+(currentPercent+1));
   }
 
-  document.getElementById("percent-span").innerHTML=currentPercent+"%";
+
+  document.getElementById("percent-span").childNodes[0].innerHTML=100-currentPercent+"%";
+  document.getElementById("percent-span").childNodes[2].innerHTML=currentPercent+"%";
+
+  document.getElementById("negInfoText").childNodes[1].innerHTML=100-currentPercent+"% des Internets sind in negativer Stimmung zum Thema "+currentTheme;
+  document.getElementById("posInfoText").childNodes[1].innerHTML=currentPercent+"% des Internets sind in positiver Stimmung zum Thema "+currentTheme;
 
   if(currentPercent==percentToSet){
     console.log("Percent set!");
