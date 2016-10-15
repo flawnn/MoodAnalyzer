ee2f3617844cff0fa0c411d72470e0c223f7195b
NightScap3 <baran.peters@lessing-ffm.net>
2016-10-15 17:19:16 +0200

    Reworked

--- mood_analyzer/twitter_interface.py
+++ mood_analyzer/twitter_interface.py
@@ -1,24 +1,33 @@
 from loklak import Loklak
+import datetime
+
 
 loklak = Loklak()
-def get_tweets(hashtag):
+def get_tweets(hashtag, since=14, count=500):
     """
     Fetches all tweets to a given hashtag
     :param hashtag: (str) keyword
+    :param since: (str) Y-M-D
+    :param count: (int) count
     :return: (list) list of json strings with content
     """
+    now = datetime.datetime.now()
+    since = now + datetime.timedelta(days=-since)
+    since = datetime.datetime.strftime(since, "%Y-%m-%d")
+    print(since)
     tweet_list = []
 
-    for t in loklak.search(hashtag)["statuses"]:
+    for t in loklak.search(hashtag, since=since, count=count)["statuses"]:
+        """Parameters: self, query=None, since=None, until=None, from_user=None, count=None """
         tweet_list.append(t["text"])
 
-    return tweet_list
-
-def get_status(self): 
-
-    print(loklak.status())
+    return tweet_list, len(tweet_list), since
 
 
 if __name__ == "__main__":
-    for tweet in get_tweets('jugendhackt'):
-        print(tweet)
+    counter = 0
+    for tweets in get_tweets("Trump"):
+        counter += 1
+        print(tweets, counter, )
+
+
